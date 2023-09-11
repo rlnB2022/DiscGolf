451540a7f36c1e0ae7b0736ad551d13229e07f38
rlnB2022 <beyer.richard73@gmail.com>
2023-09-11 16:06:31 -0500

    Working top nav bar animation that slides to selected mobile nav item when clicked

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,23 @@ const mobileNavButtons = document.querySelectorAll(
 	"nav.mobile-navbar > ul > li"
 );
 
+const mobileNavTopBar = document.querySelector(".menu-selector");
+
 /**
  * Assign click event listener to each mobile menu item
  *
  */
-for (const button of mobileNavButtons) {
-	button.addEventListener("click", () => {
+for (const [index, button] of mobileNavButtons.entries()) {
+	button.addEventListener("click", (e) => {
 		mobileNavButtons.forEach((btn) => {
 			/*  */
 			if (btn !== button) {
 				btn.classList.remove("menu-active");
+				mobileNavTopBar.className = "";
+				mobileNavTopBar.classList.add("menu-selector");
+				mobileNavTopBar.classList.add("menu-" + index);
+				// if (mobileNavTopBar.classList.add("menu-" + index)) {
+				// }
 			}
 		});
 		button.classList.add("menu-active");
