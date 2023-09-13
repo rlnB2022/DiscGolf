1c8153e4a4f4c02ef889888bf75b9a2d448d0def
rlnB2022 <beyer.richard73@gmail.com>
2023-09-12 22:05:10 -0500

    Created menu that animates in from the left

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const line1 = document.getElementById("line-1");
 const line2 = document.getElementById("line-2");
 const line3 = document.getElementById("line-3");
 
+const mobileNav = document.querySelector(".mobile-nav");
+
 /**
  * 1. Set click handler for hamburger button
  * 2. Animate hamburger lines to show X or three lines (animated)
@@ -12,4 +14,6 @@ hamburgerButton.addEventListener("click", () => {
 	line1.classList.toggle("rotate-down");
 	line2.classList.toggle("line-hidden");
 	line3.classList.toggle("rotate-up");
+
+	mobileNav.classList.toggle("active");
 });
