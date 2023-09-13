3e976e090becfe875072dc36f1a1493f869d9c59
rlnB2022 <beyer.richard73@gmail.com>
2023-09-12 21:02:18 -0500

    Client requested a hamburger menu instead of bottom buttons after all. Beginning to make those changes.

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-	const body = document.body;
-
-	const navTarget = document.querySelector(
-		"li#" + body.id.split("").slice(4).join("")
-	);
-
-	navTarget.classList.add("menu-active");
-}, 100);
