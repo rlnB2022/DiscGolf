d3b549801513af221eb5353e8c8606d7f7da7949
rlnB2022 <beyer.richard73@gmail.com>
2023-09-12 21:20:17 -0500

    hamburger lines now animate

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const hamburgerButton = document.querySelector(".hamburger-container");
+const line1 = document.getElementById("line-1");
+const line2 = document.getElementById("line-2");
+const line3 = document.getElementById("line-3");
+
+/**
+ * 1. Set click handler for hamburger button
+ * 2. Animate hamburger lines to show X or three lines (animated)
+ * 3. Toggle menu
+ */
+hamburgerButton.addEventListener("click", () => {
+	line1.classList.toggle("rotate-down");
+	line2.classList.toggle("line-hidden");
+	line3.classList.toggle("rotate-up");
+});
